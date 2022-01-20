929279bda4d513d1830a0c79dd63f9dad45c9f7d
Agnese Spinella <agnesespinella@gmail.com>
2022-01-20 10:11:53 +0100

    gallery fixed

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,12 +1,22 @@
 import LayoutDefault from '../Layouts/index'
 import Image from 'next/image'
+import { useEffect,useState } from 'react'
+
+const loremPicsum = [
+    "https://picsum.photos/1501/400",
+    "https://picsum.photos/1502/400",
+    "https://picsum.photos/1500/400"
+]
 
 export default function Gallery() {
-    return (
-        <LayoutDefault>
-            <h3>Questa è la gallery</h3>
-            <Image src="https://picsum.photos/1500/400" alt="pic" width={1500} height={400}/>
-        </LayoutDefault>
-    )
-}
+    const [images, setImages] = useState([])
 
+useEffect(() => {
+    setImages(loremPicsum)
+}, [])
+
+    return (images.map((img) => (
+    <Image src={img} alt={img} width={1500} height={400}/>
+    ))
+    )
+    }
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,9 +1,11 @@
 import LayoutDefault from '../Layouts/index'
 import styles from "../styles/Home.module.css"
+import Gallery from './gallery'
 
 export default function Home() {
     return (
         <LayoutDefault>
+
             <h1 className={styles.container}>Pagina di apertura</h1>
             <p> Primo progetto in Next.js</p>
         </LayoutDefault>
